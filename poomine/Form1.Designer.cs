2617f2434812b596dd15465c31701323e456eaaf
Kevin Uueni <kevin.uueni@tptlive.ee>
2018-01-12 08:23:00 +0200

    Poomine

---
+++ poomine/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace poomine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_Length = new System.Windows.Forms.Label();
+            this.lbl_Guesses = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbl_Missed = new System.Windows.Forms.Label();
+            this.txt_Word = new System.Windows.Forms.TextBox();
+            this.txt_Letter = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_Letter = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(423, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 306);
+            this.panel1.TabIndex = 7;
+            // 
+            // lbl_Length
+            // 
+            this.lbl_Length.AutoSize = true;
+            this.lbl_Length.Location = new System.Drawing.Point(3, 249);
+            this.lbl_Length.Name = "lbl_Length";
+            this.lbl_Length.Size = new System.Drawing.Size(78, 15);
+            this.lbl_Length.TabIndex = 8;
+            this.lbl_Length.Text = "Sõna pikkus: ";
+            // 
+            // lbl_Guesses
+            // 
+            this.lbl_Guesses.AutoSize = true;
+            this.lbl_Guesses.Location = new System.Drawing.Point(150, 249);
+            this.lbl_Guesses.Name = "lbl_Guesses";
+            this.lbl_Guesses.Size = new System.Drawing.Size(87, 15);
+            this.lbl_Guesses.TabIndex = 9;
+            this.lbl_Guesses.Text = "Arvamisi järgi: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_Word);
+            this.groupBox2.Controls.Add(this.lbl_Missed);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txt_Letter);
+            this.groupBox2.Controls.Add(this.lbl_Length);
+            this.groupBox2.Controls.Add(this.lbl_Guesses);
+            this.groupBox2.Controls.Add(this.btn_Letter);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(385, 369);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // lbl_Missed
+            // 
+            this.lbl_Missed.AutoSize = true;
+            this.lbl_Missed.Location = new System.Drawing.Point(3, 281);
+            this.lbl_Missed.Name = "lbl_Missed";
+            this.lbl_Missed.Size = new System.Drawing.Size(125, 15);
+            this.lbl_Missed.TabIndex = 9;
+            this.lbl_Missed.Text = "Valesti arvatud tähed: ";
+            this.lbl_Missed.Click += new System.EventHandler(this.lbl_Missed_Click);
+            // 
+            // txt_Word
+            // 
+            this.txt_Word.Location = new System.Drawing.Point(99, 337);
+            this.txt_Word.Name = "txt_Word";
+            this.txt_Word.Size = new System.Drawing.Size(136, 22);
+            this.txt_Word.TabIndex = 13;
+            // 
+            // txt_Letter
+            // 
+            this.txt_Letter.Location = new System.Drawing.Point(101, 304);
+            this.txt_Letter.Name = "txt_Letter";
+            this.txt_Letter.Size = new System.Drawing.Size(32, 22);
+            this.txt_Letter.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 27);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Arva sõna";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_Letter
+            // 
+            this.btn_Letter.Location = new System.Drawing.Point(6, 299);
+            this.btn_Letter.Name = "btn_Letter";
+            this.btn_Letter.Size = new System.Drawing.Size(87, 27);
+            this.btn_Letter.TabIndex = 10;
+            this.btn_Letter.Text = "Arva täht";
+            this.btn_Letter.UseVisualStyleBackColor = true;
+            this.btn_Letter.Click += new System.EventHandler(this.btn_Letter_Click_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(515, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(610, 324);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 57);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 391);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Poomine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_Guesses;
+        private System.Windows.Forms.Label lbl_Length;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_Word;
+        private System.Windows.Forms.TextBox txt_Letter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_Letter;
+        private System.Windows.Forms.Label lbl_Missed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ poomine/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace poomine
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Enne mängimise alustamist vajuta START nuppu, et mäng õigesti töötaks!", "START", MessageBoxButtons.OK);
+        }
+        
+
+        string word = "";
+        List<Label> labels = new List<Label>();
+        int amount = 0;
+        int guessesLeft = 9;
+
+        enum BodyParts
+        {
+            Head,
+            Left_Eye,
+            Right_Eye,
+            Mouth,
+            Body,
+            Right_Arm,
+            Left_Arm,
+            Right_Leg,
+            left_Leg
+        }
+
+        void DrawHangPost()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Black, 10);
+            g.DrawLine(p, new Point(198, 321), new Point(198, 5));
+            g.DrawLine(p, new Point(203, 5), new Point(80, 5));
+            g.DrawLine(p, new Point(80, 0), new Point(80, 70));
+            GetRandomWord();
+        }
+
+        void DrawBodyParts(BodyParts bp)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Brown, 2);
+            if(bp == BodyParts.Head)
+            {
+                g.DrawEllipse(p, 55, 70, 55, 55);
+            }
+            else if(bp == BodyParts.Left_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Brown);
+                g.FillEllipse(s, 70, 85, 5, 5);
+            }
+            else if(bp == BodyParts.Right_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Brown);
+                g.FillEllipse(s, 90, 85, 5, 5);
+            }
+            else if(bp == BodyParts.Mouth)
+            {
+                g.DrawArc(p, 63, 100, 40, 10, 25, 360);
+            }
+            else if(bp == BodyParts.Body)
+            {
+                g.DrawLine(p, new Point(83, 127), new Point(83, 240));
+            }
+            else if(bp == BodyParts.Left_Arm)
+            {
+                g.DrawLine(p, new Point(83, 150), new Point(53, 120));
+            }
+            else if(bp == BodyParts.Right_Arm)
+            {
+                g.DrawLine(p, new Point(83, 150), new Point(112, 120));
+            }
+            else if(bp == BodyParts.left_Leg)
+            {
+                g.DrawLine(p, new Point(83, 240), new Point(62, 275));
+            }
+            else if(bp == BodyParts.Right_Leg)
+            {
+                g.DrawLine(p, new Point(83, 240), new Point(105, 275));
+            }
+
+        }
+        void MakeLabels()
+        {
+            word = GetRandomWord();
+            char[] chars = word.ToCharArray();
+            int between = 250 / chars.Length;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                labels.Add(new Label()); 
+                labels[i].Location = new Point((i * between) + 55, 150); 
+                labels[i].Text = "___"; 
+                labels[i].Parent = groupBox2; 
+                labels[i].BringToFront(); 
+                labels[i].CreateControl(); 
+            }
+            lbl_Length.Text = "Sõna pikkus: " + (chars.Length).ToString();
+            lbl_Guesses.Text = "Arvamisi järgi: " + guessesLeft;
+
+        }
+        string GetRandomWord()
+        {
+            string[] WordList = File.ReadAllLines(@"C:\Users\opilane\Documents\kevin\poomine\Praktika\sõnad.txt");
+            string[] words = WordList;
+            Random ran = new Random();
+            return words[ran.Next(0, words.Length - 1)];
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            DrawHangPost();
+            MakeLabels();
+        }
+        List<string> guessedLetters = new List<string>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(txt_Word.Text == word)
+            {
+                MessageBox.Show("Palju õnne! Te võitsite mängu!", "Palju Õnne!", MessageBoxButtons.OK);
+                Environment.Exit(1);
+            }
+            else
+            {
+                MessageBox.Show("Sõna mille valisite on vale!");
+                DrawBodyParts((BodyParts)amount);
+                amount++;
+                guessesLeft--;
+                lbl_Guesses.Text = "Arvamisi järgi: " + guessesLeft;
+                if(amount == 9)
+                {
+                    MessageBox.Show("Teie valikud said otsa! Sõna oli " + word, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(1);
+                }
+            }
+
+        }
+        void ResetGame()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(panel1.BackColor);
+            GetRandomWord();
+            MakeLabels();
+            DrawHangPost();
+            lbl_Missed.Text = "Valesti arvatud tähed:  ";
+            txt_Letter.Text = "";
+            txt_Word.Text = "";
+            guessesLeft = 9;
+            amount = 0;
+            lbl_Guesses.Text = "Arvamisi järgi: ";
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lbl_Missed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void btn_Letter_Click_Click_1(object sender, EventArgs e)
+        {
+            Convert.ToChar(txt_Letter.Text);
+            char userletter = txt_Letter.Text.ToLower().ToCharArray()[0];
+            bool letCheck = false;
+            bool multCheck = false;
+            string convUsrLet;
+            if (!char.IsLetter(userletter))
+            {
+                MessageBox.Show("Sisestada tuleb ainult tähti!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                letCheck = true;
+            }
+            else
+            {
+                convUsrLet = userletter.ToString();
+                if (guessedLetters.Contains(convUsrLet))
+                {
+                    multCheck = true;
+                }
+
+                if (multCheck == true)
+                {
+                    MessageBox.Show("Te olete selle tähe juba ära arvanud!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    multCheck = false;
+                }
+                else if (letCheck == true)
+                {
+                    MessageBox.Show("Palun sisestage mingi täht!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    guessedLetters.Add(convUsrLet);
+                    if (word.Contains(userletter))
+                    {
+                        char[] letters = word.ToCharArray();
+                        for (int i = 0; i < letters.Length; i++)
+                        {
+                            if (letters[i] == userletter)
+                                labels[i].Text = userletter.ToString();
+                        }
+                        foreach (Label l in labels)
+                            if (l.Text == "___") return;
+                        MessageBox.Show("Palju õnne! Te võitsite mängu!", "Palju Õnne", MessageBoxButtons.OK);
+                        Environment.Exit(1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Täht mille valisite ei ole sõnas sees!");
+                        lbl_Missed.Text += " " + userletter.ToString() + ",";
+                        DrawBodyParts((BodyParts)amount);
+                        amount++;
+                        guessesLeft--;
+                        lbl_Guesses.Text = "Arvamisi järgi: " + guessesLeft;
+                        if (amount == 9)
+                        {
+                            MessageBox.Show("Tegite liiga palju valesid valikuid! Sõna oli: " + word, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            Environment.Exit(1);
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
